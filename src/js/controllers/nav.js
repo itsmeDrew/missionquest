77bb4c015b93fd42cd40b2d69b7636acf4598ca1
Drew Roberts <drew.roberts@methodfactory.com>
2015-07-11 14:44:08 -0400

    Mobile menu bug fix

--- src/js/controllers/nav.js
+++ src/js/controllers/nav.js
@@ -14,16 +14,46 @@ define(
     function NavController($scope, $rootScope, $location, Category) {
       var vm = this;
       var mobileMenuActive = $('body').hasClass('mobile-menu--active');
+      var $windowEl = $(window);
+      var breakPoint = 768;
 
       vm.categories = [];
       vm.getChildren = getChildren;
       vm.toggleMenu = toggleMenu;
+      $rootScope.$mobile = false;
 
-      updateCategories();
+      init();
+
+      function init() {
+        updateCategories();
+        responsiveCheck();
+
+        if ( $windowEl.width() < breakPoint ) {
+          $rootScope.$mobile = true;
+        }
+
+      }
+
+      function responsiveCheck() {
+        //setting rootscope variable for mobile.
+        $windowEl.resize(function () {
+          if ( $windowEl.width() < breakPoint ) {
+            if (! $rootScope.$mobile) {
+              $rootScope.$mobile = true;
+            }
+          } else {
+            if ($rootScope.$mobile) {
+              $rootScope.$mobile = false;
+            }
+          }
+        })
+      }
 
       function toggleMenu() {
         //only toggle the menu if you are in the mobile menu
-        mobileMenuActive ? $scope.$emit('menu.toggle') : mobileMenuActive = false;
+        if ($rootScope.$mobile) {
+          $scope.$emit('menu.toggle');
+        }
       }
 
       function updateCategories () {

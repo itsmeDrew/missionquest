cc32974408a8833fd195176ef063d93c070d1068
Drew Roberts <drew.roberts@methodfactory.com>
2015-08-05 16:06:25 -0400

    updated functions

--- public/api/wp-content/themes/twentyfifteen/functions.php
+++ public/api/wp-content/themes/twentyfifteen/functions.php
@@ -360,10 +360,13 @@ require get_template_directory() . '/inc/customizer.php';
 
 function wp_api_encode_acf($data,$post,$context){
     $customMeta = (array) get_fields($post['ID']);
+    $data['custom_fields'] = array();
 
-    $data['custom_fields'] = array_filter($customMeta, function($key, $value) {
-    	return $key != '';
-    }, ARRAY_FILTER_USE_BOTH);
+    foreach ($customMeta as $key => $value) {
+    	if ($key != '') {
+    		$data['custom_fields'][$key] = $value;
+    	}
+    }
 
     return $data;
 }
